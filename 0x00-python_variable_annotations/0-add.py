69f434ba23007a7bc41db97e4479fa52de1d466d
Orjihenry <orjihen@yahoo.com>
2023-12-07 21:52:54 +0100

    Basic annotations - add ✌️ 😌⭐

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+Adds two variables and returns a value.
+Indicates that both  params are floating numbers,
+and ensures that the return value is a floating number.
+"""
+
+
+def add(a: float, b: float) -> float:
+    return a + b
