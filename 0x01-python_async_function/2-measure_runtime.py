e9d90df6f41a38152460e8a7df1a62fa240fbac9
Orjihenry <orjihen@yahoo.com>
2023-12-14 14:19:01 +0100

    Measures the runtime 😜 🎉 💯

---
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+'''
+Measures the runtime.
+'''
+import time
+import asyncio
+
+
+wait_n = __import__('1-concurrent_coroutines').wait_n
+
+
+async def measure_time(n: int, max_delay: int) -> float:
+    ''' measures the average runtiome of wait_n. '''
+    startTime = time.time()
+    asyncio.run(wait_n(n, max_delay))
+    total_time = time.time() - startTime
+    return total_time / n
